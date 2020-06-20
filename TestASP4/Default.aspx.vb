5c09bfa5ce227f432f57bbd92def64b8834bcc49
Somnoboy <66186271+Somnoboy@users.noreply.github.com>
2020-06-20 01:19:07 -0400

    Add project files.

---
+++ TestASP4/Default.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub btnTry_Click(sender As Object, e As EventArgs) Handles btnTry.Click
+        lblTry.Text = "It worked."
+    End Sub
+End Class
